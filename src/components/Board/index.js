6468094a31e079d6bc09df0c6bb7a1c1d4253811
pedrosilva <pedrogdsilva410@gmail.com>
2020-07-15 16:38:16 +0100

    tictactoe game completion with logic, fully working (need to implement tie)

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -8,15 +8,9 @@ const Board = ({squares, onClick}) => {
     <Fragment>
     <h1 className="title">TIC TAC TOE</h1>
     <div className="board">
-        <Square value="1" onClick={()=> onClick(" dummy value")}/>
-        <Square value="2" onClick={()=> onClick(" dummy value")}/>
-        <Square value="3" onClick={()=> onClick(" dummy value")}/>
-        <Square value="4" onClick={()=> onClick(" dummy value")}/>
-        <Square value="5" onClick={()=> onClick(" dummy value")}/>
-        <Square value="6" onClick={()=> onClick(" dummy value")}/>
-        <Square value="7" onClick={()=> onClick(" dummy value")}/>
-        <Square value="8" onClick={()=> onClick(" dummy value")}/>
-        <Square value="9" onClick={()=> onClick(" dummy value")}/>
+          {squares.map((square, i) => (
+            <Square key={i} value={square} onClick={() => onClick(i)} />
+          ))}
     </div>
     </Fragment>
   );
--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -1,24 +1,38 @@
-import React from "react";
+import React, {useState, Fragment} from "react"
+import calculateWinner from "../../js/calculateWinner"
 import Board from "../Board"
 
-import "./styles.css";
+import "./styles.css"
 
 const Game = () => {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+  const winner = calculateWinner(board);
 
-  const handleClick = () => {
-        
+  const handleClick = i => {
+    const boardCopy = [...board];
+    // If user clicks an occupied square or if game is won, return
+    if (winner || boardCopy[i]) return;
+    // decide wehtehr to put an X or an O in the clicked square
+    boardCopy[i] = xIsNext ? 'X' : 'O';
+    setBoard(boardCopy);
+    setXIsNext(!xIsNext);
   }
-
-  // const jumpTo = () => {
-        
-  // }
   
-  // const renderMoves = () => {
-      
-  // }
+  const renderMoves = () => (
+    <button onClick={() => setBoard(Array(9).fill(null))}>
+    Start Game
+    </button>
+  )
   
   return (
-      <Board onClick={handleClick} />
+    <Fragment>
+    <Board squares={board} onClick={handleClick} />
+    <div className="boardInfo">
+        <p>{winner ? 'Winner: ' + winner : 'Next Player: ' + (xIsNext ? 'X' : 'O')}</p>
+        {renderMoves()}
+    </div>
+    </Fragment>
   )
 }
 
