68052594461743cde554abf085c843996c7ec5a3
pedrosilva <pedrogdsilva410@gmail.com>
2020-07-16 11:57:50 +0100

    build

---
+++ build/precache-manifest.ba209457cc3ae3a852b1244438c8fb00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01724254dbd2ff7e2324c3dec1026c9e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "061917583be423bb004c",
+    "url": "/static/css/main.0c82e2c2.chunk.css"
+  },
+  {
+    "revision": "e03320116f319fc6b183",
+    "url": "/static/js/2.b0ac0977.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.b0ac0977.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "061917583be423bb004c",
+    "url": "/static/js/main.b7c36f8f.chunk.js"
+  },
+  {
+    "revision": "c2f98a5c387da539a513",
+    "url": "/static/js/runtime-main.5e682c5b.js"
+  }
+]);
