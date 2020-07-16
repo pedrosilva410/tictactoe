046ed5782c3684e7647a8fc9e7341132aaf0a8dc
pedrosilva <pedrogdsilva410@gmail.com>
2020-07-16 11:42:08 +0100

    feat: tie; also did some styling

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -1,5 +1,5 @@
-import React, {useState, Fragment} from "react"
-import calculateWinner from "../../js/calculateWinner"
+import React, { useState, Fragment } from "react"
+import calculateResult from "../../js/calculateResult"
 import Board from "../Board"
 
 import "./styles.css"
@@ -7,31 +7,49 @@ import "./styles.css"
 const Game = () => {
   const [board, setBoard] = useState(Array(9).fill(null));
   const [xIsNext, setXIsNext] = useState(true);
-  const winner = calculateWinner(board);
+  const result = calculateResult(board);
 
   const handleClick = i => {
     const boardCopy = [...board];
     // If user clicks an occupied square or if game is won, return
-    if (winner || boardCopy[i]) return;
+    if (result || boardCopy[i]) return;
     // decide wehtehr to put an X or an O in the clicked square
     boardCopy[i] = xIsNext ? 'X' : 'O';
     setBoard(boardCopy);
     setXIsNext(!xIsNext);
   }
-  
-  const renderMoves = () => (
-    <button onClick={() => setBoard(Array(9).fill(null))}>
-    Start Game
-    </button>
-  )
-  
+
+  const renderMoves = () => {
+    if (board.every(square => square === null)) {
+      return null
+    } else {
+      return (
+        <button className="restartGameBtn" onClick={() => setBoard(Array(9).fill(null))}>
+          Restart Game
+        </button>
+      )
+    }
+  }
+
+  const renderText = () => {
+    if (result && result !== "tie") {
+      return `Winner: ${result}`
+    } else if (result && result === "tie") {
+      return "It's a tie"
+    } else if(board.every(square => square === null)){
+      return `First Player: ${(xIsNext ? 'X' : 'O')}`
+    } else {
+      return `Next Player: ${(xIsNext ? 'X' : 'O')}`
+    }
+  }
+
   return (
     <Fragment>
-    <Board squares={board} onClick={handleClick} />
-    <div className="boardInfo">
-        <p>{winner ? 'Winner: ' + winner : 'Next Player: ' + (xIsNext ? 'X' : 'O')}</p>
+      <Board squares={board} onClick={handleClick} />
+      <div className="boardInfo">
+        <p className="turnText">{renderText()}</p>
         {renderMoves()}
-    </div>
+      </div>
     </Fragment>
   )
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./styles.css";
-import Game from "../../components/Game"
 
 const Home = () => (
-    <main>
-      <Game/>
-    </main>
-  );
+  <main>
+    <h1 className="home__title">Tic Tac Toe</h1>
+    <p className="home__text">Welcome to my Tic Tac Toe.</p>
+    <p className="home__text">Let's PLay? (maybe read about first)</p>
+      <Link className="playButton" to="/tictactoe">
+        Click me to play
+      </Link>
+  </main>
+);
 
 export default Home;
+
