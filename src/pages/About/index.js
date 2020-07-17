d6a826dfb25508a1cfd432f955edaa5c0eb5fdbd
pedrosilva <pedrogdsilva410@gmail.com>
2020-07-17 10:18:51 +0100

    fix: removed about from gitignore

---
+++ src/pages/About/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./styles.css";
+
+const About = () => {
+  return (
+    <main>
+      <h1 className="about__title">About</h1>
+      <p className="about__text">
+        This is a project I did for fun and to learn more about react, in this
+        version You can only play against yourself, or a friend next to you
+        using the same computer, I hope you enjoy :)
+      </p>
+    </main>
+  );
+};
+
+export default About;
